1bd1df466d94b7c7976f0316795f6e8ff621127c
PiotrKosiara <92859167+PiotrKosiara@users.noreply.github.com>
2024-11-14 23:00:31 +0100

    added functionality

--- webscraper.py
+++ webscraper.py
@@ -3,21 +3,24 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
 all_data = []
 
 # Przechodzenie przez konkretne regiony (na stronie wyszczególnione są 4)
 # Można do finalnej tabeli dodać kolumne Region i wpisywać tam odpowiednią nazwę
-for region in range(1, 5):
+for region in range(1, 2):
     page = 1
     while True:
         # warunek na blokowanie wczytaniu ostatnich stron w danym regionie
-        if page == 5:
+        if page == 2:
             break
 
             # Strona url zmienia w konkretnym miejscu liczbe region i strone
         url = f"https://www.e-drewno.pl/stock/?product=stock&module=auctions&action=showAuctions&options=all&region={region}&page={page}"
-
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
+        response.encoding = 'utf-8'
         if response.status_code != 200:
             print(f"Failed to retrieve page {page} for region {region}")
             break
@@ -34,6 +37,25 @@ for region in range(1, 5):
 
         for row in data_rows:
             cols = row.find_all("td")
+            # Tworzenie linkudo strony na której znajduje się większa ilość danych o aukcji
+            link = row.find('a', href=True)
+            link = link.get('href')
+            full_path = "https://www.e-drewno.pl/stock/" + link
+            full_path = full_path.replace('®', '&reg')
+            response_2 = requests.get(full_path, headers=headers)
+            if response_2.status_code != 200:
+                print(f"Failed to retrieve specific data")
+            soup_2 = BeautifulSoup(response_2.text, 'html.parser')
+            table_2 = soup_2.find("table", class_='okienko')
+            cols_2 = table_2.find_all("td", class_='aukcja_legend')
+            print(cols_2)
+            table_table = table_2.find("table", class_='okienko')
+            # if table_table:
+            # print(table_table.find_all('tr'))
+            # print(table_table.find_all('td'))
+            # print(table_table.find_all('b'))
+            # print(table_table.find('tbody'))
+
             if len(cols) >= 11:
                 data = {
                     "Lp": cols[0].text.strip(),
@@ -46,7 +68,13 @@ for region in range(1, 5):
                     "Ilość ofert złożonych/wygrywających": cols[7].text.strip(),
                     "Cena otwarcia netto": cols[8].text.strip(),
                     "Teraz wygrywa": cols[9].text.strip(),
-                    "Do końca pozostało": cols[10].text.strip()
+                    "Do końca pozostało": cols[10].text.strip(),
+                    "Opis aukcji": cols_2[0].text.strip(),
+                    "Termin na podpisanie umowy": table_2.find("td",
+                                                               class_='aukcja_legend_agreementdeadline').text.strip(),
+                    "Termin odbioru drewna": cols_2[1].text.strip(),
+                    "Tel. kontaktowy": cols_2[2].text.strip(),
+                    # "Przewidywany procentowy udział klas jakości i grubości": str(table_table.find_all('tr')),
                 }
                 all_data.append(data)
 
